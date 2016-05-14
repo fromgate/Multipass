d6b1d75083dcf6e1f0f36cd2f7dfb293fbbed4c5
fromgate <fromgate@gmail.com>
2016-05-14 20:29:08 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Multipass
-
 Permissions plugin for Nukkit approved by Leeloo Dallas
+
+## Main principles
+Multipass is a permissions plugin for Nukkit that brings a universal permission system to nukkits.
+
--- src/main/java/ru/nukkit/multipass/Multipass.java
+++ src/main/java/ru/nukkit/multipass/Multipass.java
@@ -3,14 +3,59 @@ package ru.nukkit.multipass;
 import cn.nukkit.Player;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Igor on 12.05.2016.
  */
 public class Multipass {
 
-    public static List<String> getPlayerGroups(Player player) {
+    public static List<String> getGroups(Player player) {
         return null;
+    }
+
+    public static Map<String,Integer> getPrefixes (Player player){
+        return null;
+    }
+
+    public static Map<String,Integer> getSuffixes (Player player){
+        return null;
+    }
+
+    public static String getPrefix(Player player){
+        return null;
+    }
+
+    public static String getSuffix(Player player){
+        return null;
+    }
+
+    public boolean isInGroup(Player player){
+        return false;
+    }
+
+    public boolean setPermission (Player player, String permission){
+        return false;
+    }
+
+    public void setGroup (Player player, String group){
+
+    }
+
+    public void removePermission (Player player, String permission){
+
+    }
+
+    public void removeGroup (Player player, String group){
+
+    }
+
+    public void setPlayerPrefix (Player player, String prefix, String suffix, int priority){
+
+    }
+
+    public void setGroupPrefix (String group, String prefix, int priority){
+
     }
 
 
